99eabb3c0c7fc91932e08c54527e440e6550df61
relaximus <alche8411@gmail.com>
2020-10-27 23:40:13 +0100

    loggers example with controller

---
+++ src/main/java/com/relaximus/actuatortest/DemoController.java
@@ -0,0 +1,17 @@
+package com.relaximus.actuatortest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class DemoController {
+    @GetMapping("/push-log")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void pushLog() {
+        log.debug("This is a debug log from DemoController");
+    }
+}
