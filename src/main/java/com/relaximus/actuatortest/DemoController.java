ff889ecf92dea01f5fbabead0db66b8f0a9352fb
relaximus <alche8411@gmail.com>
2020-10-29 23:36:04 +0100

    metrics

--- src/main/java/com/relaximus/actuatortest/DemoController.java
+++ src/main/java/com/relaximus/actuatortest/DemoController.java
@@ -1,5 +1,6 @@
 package com.relaximus.actuatortest;
 
+import io.micrometer.core.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/push-log")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Timed(extraTags = { "demo-tag", "test-val" })
     public void pushLog() {
         log.debug("This is a debug log from DemoController");
     }
